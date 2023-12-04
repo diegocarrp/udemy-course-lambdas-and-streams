bcd1d30e9b6d55ec6c8ca79a1d23ddbabb3909a8
diego <diegocarrp@gmail.com>
2023-12-04 19:55:22 -0300

    Ejercicios de lambdas

--- src/main/java/com/debuggeando_ideas/streams/ExerciseLambdas.java
+++ src/main/java/com/debuggeando_ideas/streams/ExerciseLambdas.java
@@ -1,53 +1,68 @@
 package com.debuggeando_ideas.streams;
 
+import com.debuggeando_ideas.util.Console;
 import com.debuggeando_ideas.util.Database;
+import com.debuggeando_ideas.util.Review;
 import com.debuggeando_ideas.util.Videogame;
 
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class ExerciseLambdas {
 
-    public static void main(String[] args) {
-        Stream<Videogame> videogames = Database.videogames.stream();
+    public static void main( String[] args ) {
+        Stream<Videogame> videogames = Database.videogames.stream( );
     }
 
     /*
-    *Regresar true si el stream contiene al menos un videojuego con número de ventas mayor a 10
-    * y no este en descuento o su precio sea mayor a 9.99 de lo contrario regresar false.
+     *Regresar true si el stream contiene al menos un videojuego con número de ventas mayor a 10
+     * y no este en descuento o su precio sea mayor a 9.99 de lo contrario regresar false.
      */
-    static Boolean exercise1(Stream<Videogame> stream) {
-      return null;
+    static Boolean exercise1( Stream<Videogame> stream ) {
+//        return stream.filter( videogame -> !videogame.getIsDiscount( ) )
+//                .filter( videogame -> videogame.getPrice( ) < 9.99 )
+//                .anyMatch( videogame -> videogame.getTotalSold( ) > 10 );
+        return  stream.anyMatch(v -> v.getTotalSold() > 10 && (!v.getIsDiscount() || v.getPrice() > 9.99));
     }
 
     /*
      *Regresar un Stream unicamente con los titulos de todas las consolas que sean de XBOX
      * durante el proceso imprimir los resultados ignorando los repetidos.
      */
-    static Stream<String> exercise2(Stream<Videogame> stream) {
-        return null;
+    static Stream<String> exercise2( Stream<Videogame> stream ) {
+        return stream
+                .distinct()
+                .filter(v -> v.getConsole().equals(Console.XBOX))
+                .peek(v -> System.out.println("Process: " + v))
+                .map(Videogame::getName);
     }
 
     /*
      *Regresar el videojuego con el mayor número de ventas
      * unicamente contemplando los primers 10 elementos del stream.
      */
-    static Videogame exercise3(Stream<Videogame> stream) {
-        return null;
+    static Videogame exercise3( Stream<Videogame> stream ) {
+        return stream.limit( 10 )
+                .max( Comparator.comparingInt( Videogame::getTotalSold ) )
+                .orElseThrow();
     }
 
     /*
      *Regresar un stream con todos los comentarios del cada review de todos los videojuegos del stream.
      */
-    static Stream<String> exercise4(Stream<Videogame> stream) {
-        return null;
+    static Stream<String> exercise4( Stream<Videogame> stream ) {
+        return stream.map( Videogame::getReviews )
+                .flatMap( reviews -> reviews.stream( ).map( Review::getComment ) );
     }
 
     /*
      *Regresar un stream con los todos los videojuegos con precio menores a 20.0
      * sin utilizar el operador filter().
      */
-    static Stream<Double> exercise5(Stream<Videogame> stream) {
-        return null;
+    static Stream<Videogame> exercise5( Stream<Videogame> stream ) {
+        return stream.sorted( Comparator.comparingDouble( Videogame::getPrice ) )
+                .takeWhile( videogame -> videogame.getPrice() < 20.0 );
     }
 
 }
